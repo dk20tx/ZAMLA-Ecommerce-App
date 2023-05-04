ace01c495ec7d4281cdc22558c1ede70d1bfd3bb
dk20tx <dismastoo96@gmail.com>
2023-05-04 20:25:28 +0300

    All files commit

---
+++ static/js/script.js
@@ -0,0 +1,66 @@
+// some scripts
+
+// jquery ready start
+$(document).ready(function() {
+	// jQuery code
+
+
+    /* ///////////////////////////////////////
+
+    THESE FOLLOWING SCRIPTS ONLY FOR BASIC USAGE,
+    For sliders, interactions and other
+
+    */ ///////////////////////////////////////
+    $("#sidebarCollapse").on("click", function() {
+    $("#sidebar").addClass("active");
+  });
+
+  $("#sidebarCollapseX").on("click", function() {
+    $("#sidebar").removeClass("active");
+  });
+
+  $("#sidebarCollapse").on("click", function() {
+    if ($("#sidebar").hasClass("active")) {
+      $(".overlay").addClass("visible");
+      console.log("it's working!");
+    }
+  });
+
+  $("#sidebarCollapseX").on("click", function() {
+    $(".overlay").removeClass("visible");
+  });
+
+
+	//////////////////////// Prevent closing from click inside dropdown
+    $(document).on('click', '.dropdown-menu', function (e) {
+      e.stopPropagation();
+    });
+
+
+    $('.js-check :radio').change(function () {
+        var check_attr_name = $(this).attr('name');
+        if ($(this).is(':checked')) {
+            $('input[name='+ check_attr_name +']').closest('.js-check').removeClass('active');
+            $(this).closest('.js-check').addClass('active');
+           
+
+        } else {
+            item.removeClass('active');
+            
+        }
+    });
+
+	if($('[data-toggle="tooltip"]').length>0) {  // check if element exists
+		$('[data-toggle="tooltip"]').tooltip()
+	} // end if
+
+
+
+
+
+});
+// jquery end
+
+setTimeout(function(){
+  $('#message').fadeOut('slow')
+}, 4000)
